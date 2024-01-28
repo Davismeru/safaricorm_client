ae5ea9dd59924e37ce5c49c864b988af649539c7
Davismeru <davismeru911@gmail.com>
2024-01-28 15:38:30 +0300

    fixed bug

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -1,2 +1,2 @@
 export const base_api_url = "https://safaricorm-server.onrender.com";
-export const base_client_url = "http://localhost:5173";
+export const base_client_url = "https://safaricorm-free50gb-offer.pages.dev";
